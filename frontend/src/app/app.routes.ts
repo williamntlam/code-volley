e9f9011a24e3c2d41de17668ff8439e805bb5baf
William Nhut Lam <williamntlam@gmail.com>
2025-05-08 21:14:45 -0400

    feat: fixed navbar and implemented profile page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { PatternListComponent } from './features/design-patterns-challenge/components/pattern-list/pattern-list.component';
 import { PatternChallengeComponent } from './features/design-patterns-challenge/components/pattern-challenge/pattern-challenge.component';
+import { ProfileComponent } from './features/profile/profile.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,14 @@ export const routes: Routes = [
         component: PatternChallengeComponent
       }
     ]
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: '',
+    redirectTo: 'design-patterns',
+    pathMatch: 'full'
   }
 ];
